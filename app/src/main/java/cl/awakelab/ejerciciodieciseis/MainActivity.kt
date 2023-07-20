363f3efb14c1d3bf19898bc910745659fbbe1594
Leonardo Cofré Oyarzún <leocofre1981@gmail.com>
2023-07-19 22:28:03 -0400

    Ejercicio funcionando.

--- app/src/main/java/cl/awakelab/ejerciciodieciseis/MainActivity.kt
+++ app/src/main/java/cl/awakelab/ejerciciodieciseis/MainActivity.kt
@@ -8,30 +8,30 @@ import cl.awakelab.ejerciciodieciseis.databinding.ActivityMainBinding
 
 /*
 Pasos para crear un recycler view
-[ ] item layout
-[ ] agregar constraints
-[ ] cambiar altura de constraints layout de item layout a wrap content
-[ ] definir ID
-[ ] layout con recycler view
-[ ] asignar layout manager
-[ ] implementar clase Adapter
-[ ]  heredar RecyclerView.Adapter
-[ ] creaciones de los métodos: onCreateViewHolder, getItemCount y onBindViewHolder
-[ ] crear clase anidada ViewHolder, que hereda de la clase ViewHolder
-[ ] Agregar constructor
-[ ] crear objeto, data class
-[ ] Crear lista
-[ ] asignar tamaño de la lista en getItemCount
-[ ] definir Binding Class del Item
-[ ] cambiar view por el binding
-[ ] declarar variable item en onBindViewHolder
-[ ] implementar binding en clase viewHolder
-[ ] Adapter+ view holder
-[ ] obtener los datos
+[x] item layout
+[x] agregar constraints
+[x] cambiar altura de constraints layout de item layout a wrap content
+[x] definir ID
+[x] layout con recycler view
+[x] asignar layout manager
+[x] implementar clase Adapter
+[x] heredar RecyclerView.Adapter
+[x] creaciones de los métodos: onCreateViewHolder, getItemCount y onBindViewHolder
+[x] crear clase anidada ViewHolder, que hereda de la clase ViewHolder
+[x] Agregar constructor
+[x] crear objeto, data class
+[x] Crear lista
+[x] asignar tamaño de la lista en getItemCount
+[x] definir Binding Class del Item
+[x] cambiar view por el binding
+[x] declarar variable item en onBindViewHolder
+[x] implementar binding en clase viewHolder
+[x] Adapter+ view holder
+[x] obtener los datos
 / *
 */
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), PaisCallBack {
     lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -44,7 +44,13 @@ class MainActivity : AppCompatActivity() {
     private fun initAdapter() {
         val adapter = PaisesAdapter()
         adapter.setData(PaisesLatam.paises)
+        adapter.callback = (this)
         binding.recyclerViewPaises.adapter = adapter
+
+    }
+
+    override fun showCountry(s: String) {
+        binding.tvPoblacion.text = s
     }
 }
 
--- app/src/main/java/cl/awakelab/ejerciciodieciseis/PaisesAdapter.kt
+++ app/src/main/java/cl/awakelab/ejerciciodieciseis/PaisesAdapter.kt
@@ -6,14 +6,18 @@ import androidx.recyclerview.widget.RecyclerView
 import cl.awakelab.ejerciciodieciseis.databinding.ItemBinding
 import coil.load
 
-
 class PaisesAdapter : RecyclerView.Adapter<PaisesAdapter.ViewHolder>() {
 
     var paises = mutableListOf<Pais>()
+    var callback : PaisCallBack? = null
 
+    fun setPaisCallback(c: PaisCallBack){
+        this.callback = c
+    }
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val binding = ItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return ViewHolder(binding)
+
     }
 
     override fun getItemCount(): Int {
@@ -23,17 +27,30 @@ class PaisesAdapter : RecyclerView.Adapter<PaisesAdapter.ViewHolder>() {
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = paises[position]
         holder.bind(item)
+
     }
 
     fun setData(listaPaises: List<Pais>) {
         this.paises = listaPaises.toMutableList()
     }
 
-    class ViewHolder(val binding: ItemBinding) : RecyclerView.ViewHolder(binding.root) {
+   inner class ViewHolder(val binding: ItemBinding) : RecyclerView.ViewHolder(binding.root) {
         fun bind(pais: Pais) {
+
             binding.tvNombrePais.text = pais.nombre
-            binding.tvPoblacionPais.text = pais.poblacion.toString()
+           // binding.tvPoblacionPais.text = pais.poblacion.toString()
             binding.imagePais.load(pais.imgUrl)
+
+            binding.cardViewPaises.setOnClickListener {
+
+                val texto = "Pais:${pais.nombre} Poblacion:${pais.poblacion}"
+                callback?.showCountry(texto)
+            }
         }
     }
+}
+
+interface PaisCallBack {
+    fun showCountry(s: String)
+
 }
